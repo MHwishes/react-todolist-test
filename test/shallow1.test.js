e50aa60adbcd0ac634c2541a49393c8852531062
Hong Ma <hma@thoughtworks.com>
2018-08-26 22:42:32 +0800

    first commit

---
+++ test/shallow1.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import { expect } from 'chai';
+import App from '../app/components/App';
+
+function shallowRender(Component) {
+  const renderer = TestUtils.createRenderer();
+  renderer.render(<Component/>);
+  return renderer.getRenderOutput();
+  // 返回浅渲染的虚拟DOM对象
+}
+
+describe('Shallow Rendering', function() {
+  it('App\'s title should be Todos', function() {
+    const app = shallowRender(App);
+    // component's shallow rendering has props.children
+    // 每一个虚拟DOM对象都有props.children属性，它包含一个数组，里面是所有的子组件。
+    expect(app.props.children[0].type).to.equal('h1');
+    expect(app.props.children[0].props.children).to.equal('Todos');
+  });
+});
